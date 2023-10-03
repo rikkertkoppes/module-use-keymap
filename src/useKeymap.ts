e1aa4c3bda34c18e61aaabf36961c05b7d691894
Rikkert Koppes <rikkert@rikkertkoppes.com>
2023-10-03 13:02:04 +0200

    update zustand

--- src/useKeymap.ts
+++ src/useKeymap.ts
@@ -1,6 +1,6 @@
 import React from "react";
-import create from "zustand";
-import shallow from "zustand/shallow";
+import { create } from "zustand";
+import { shallow } from "zustand/shallow";
 
 import { KeymapActions, KeymapState, Keymap } from "./types";
 
